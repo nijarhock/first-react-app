57b501d80465db8628ae6e8c877c851be0720772
Nijar <nijarixa@gmail.com>
2019-09-13 17:11:12 +0700

    First react App

---
+++ src/forms/EditUserForm.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react'
+
+const EditUserForm = props => {
+    const [user, setUser] = useState(props.currentUser)
+
+    const handleInputChange = event => {
+        const { name, value } = event.target
+
+        setUser({ ...user, [name]: value})
+    }
+
+    useEffect(() => {
+        setUser(props.currentUser)
+    }, [props])
+
+    return (
+        <form onSubmit={event => {
+            event.preventDefault()
+
+            props.updateUser(user.id, user)
+        }}>
+            <label>Name</label>
+            <input type="text" name="name" value={user.name} onChange={handleInputChange} />
+            <label>Username</label>
+            <input type="text" name="username" value={user.username} onChange={handleInputChange} />
+            <button>Edit User</button>
+        </form>
+    )
+}
+
+export default EditUserForm
